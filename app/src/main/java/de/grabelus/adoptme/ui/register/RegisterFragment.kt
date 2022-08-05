5249fcd254476a8a8322f9c5d916654d42f7de8d
Bartek <bartek.grabelus@gmx.net>
2022-08-05 21:17:36 +0200

    refactoring

--- app/src/main/java/de/grabelus/adoptme/ui/register/RegisterFragment.kt
+++ app/src/main/java/de/grabelus/adoptme/ui/register/RegisterFragment.kt
@@ -9,7 +9,6 @@ import androidx.fragment.app.Fragment
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
-import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -49,7 +48,7 @@ class RegisterFragment : Fragment() {
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
 
         _binding = FragmentRegisterBinding.inflate(inflater, container, false)
         return binding.root
